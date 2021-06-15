95cee9110aabe74097443b30b275183228a8cbbc
Kristina Sananikone <snk.kristina@gmail.com>
2021-06-15 14:10:37 +0200

    model Parking and Booking validations done

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,9 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :parking
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :from_hour, presence: true
+  validates :to_hour, presence: true
 end
