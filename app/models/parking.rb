93fe36d65a763fc382126f52e1e62f312da699f7
Kristina Sananikone <snk.kristina@gmail.com>
2021-06-16 12:00:25 +0200

    upload image available now via parkings/new, default image displayed if there is no uploaded image

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -1,6 +1,7 @@
 class Parking < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
+  has_one_attached :photo
 
   validates :title, presence: true
   validates :description, presence: true
